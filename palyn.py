ec7e00b3934cfd93dc8423cbb588d71e3e66985e
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-17 16:06:04 +0530

    Add files via upload
    Finding if a number is a palindrome or not

---
+++ palyn.py
@@ -0,0 +1,17 @@
+i=int(input("Enter the number: "))
+c=i
+j=0
+k=0
+print()
+while(i>0):
+    j=(i%10)
+    k=(k*10)+j
+    i=i//10
+    
+if k==c:
+    print("It is a palyndrom")
+
+else:
+    print("It is not a palyndrom")
+
+    print(k)
