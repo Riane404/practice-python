5f59aca7866615f90c376fd14b3043a772957ddc
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-17 16:07:25 +0530

    Add files via upload
    Finding the square, square root, cube, cube root of a number

---
+++ square & root.py
@@ -0,0 +1,13 @@
+i=float(input("Enter a value: "))
+
+j=i**2
+print("The square of the number is: ",j)
+
+j=i**0.5
+print("The square root of the number is: ",j)
+
+j=i**3
+print("The cube of the number is: ",j)
+
+j=i**(1/3)
+print("The cube root of the number is: ",j)
