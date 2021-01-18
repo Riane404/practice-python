165031088f1599306fb8692a1a4b0bc2eaf202e2
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-18 16:20:56 +0530

    Add files via upload
    Which is the greater among 2 numbers

---
+++ greater 2.py
@@ -0,0 +1,12 @@
+i=float(input("Enter the first value: "))
+j=float(input("Enter the second value: "))
+print(" ")
+
+if i>j:
+    print(i,">",j)
+
+elif j>i:
+    print(j,">",i)
+
+else :
+    print ("they are equal")
