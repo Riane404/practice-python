0f3cb185ab225902d2259a6fe041f159693ecf53
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-19 19:58:14 +0530

    Add files via upload
    Finding the first 20 even numbers and their sum

---
+++ sum 20 even.py
@@ -0,0 +1,17 @@
+i=1
+s=1
+j=0
+
+while(s<=20):
+    if (i%2)==0:
+        print(i,end=" ")
+        j+=i
+        s+=1
+
+    else:
+        pass
+    i+=1
+print()
+print("The sum is: ",j)
+    
+        
