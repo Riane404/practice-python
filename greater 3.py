ae4f7dd00ecb54dddaa7e1db0f3ab2ef85d62f0f
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-19 19:06:35 +0530

    Add files via upload
    Greater among 3 numbers

---
+++ greater 3.py
@@ -0,0 +1,12 @@
+i=float(input("enter the 1st val: "))
+j=float(input("enter the 2nd val: "))
+k=float(input("enter the 3rd val: "))
+
+if i>j>k:
+    print(i," is the biggest val")
+
+elif j>k>i:
+    print(j," is the biggest val")
+
+elif k>j>i:
+    print(k," is the biggest val")
