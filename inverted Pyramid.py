547221339a96b8a8db627a073bd07fbeac28537e
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-20 09:56:32 +0530

    Add files via upload
    Printing an inverted Pyramid

---
+++ inverted Pyramid.py
@@ -0,0 +1,17 @@
+i=5
+k=0
+s=0
+t=0
+for j in range(0,i):
+    s=i-j
+    k=s
+    for c in range(0,s):
+        print("* ",end="")
+   
+    print()
+    while ((k>=s)and(k<=i)):
+        print("_" ,end="")
+        k+=1
+
+    
+    
