53f09e1334396c6ae0db09eb252291b84eda1820
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-20 10:32:56 +0530

    Add files via upload
    Takes number of values from one string separated by, and removes comma to save as individual value in list

---
+++ remove comma from list.py
@@ -0,0 +1,11 @@
+i=input("Enter the values: ")
+l=i.split(",")
+t=tuple(l)
+print()
+print("List:",end="")
+for k in l:
+    print(k,end=" ")
+print()
+print("Tuple:",end="")
+for s in t:
+    print(s,end=" ")
