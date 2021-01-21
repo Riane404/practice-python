42f68c93bd576df98134a49ebb79eeab7827713b
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-21 10:27:44 +0530

    Add files via upload
    Checks if a specific value is there in a list

---
+++ checks val in list.py
@@ -0,0 +1,6 @@
+i=float(input("Enter the number you wish to check: "))
+j=[1,4,6,2,66,7777,43,63]
+if i in j:
+    print(i,"is present in the list")
+else:
+    print(i,"is not present in the list")
