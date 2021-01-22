70c25cb020cd0c1c47bd590cf2c133635257fd2b
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-01-22 11:24:31 +0530

    Add files via upload
    Prints the longest element in the list as a string

---
+++ length of value in list.py
@@ -0,0 +1,18 @@
+i=int(input("Enter the number of values to be entered: "))
+l=list()
+v1=0
+c=0
+q=0
+q1=0
+for j in range(0,i):
+    e=input("Enter the values: ")
+    l.append(e)
+
+for i in l:
+    q=len(i)
+    
+    if q1<q:
+        c=l.index(i)
+        q1=len(i)
+    
+print("The longest value is",l[c])
