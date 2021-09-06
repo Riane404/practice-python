cd40306cd7c0ba0d6a66006c51febac487cf3f9e
Riane9029 <69118716+Riane9029@users.noreply.github.com>
2021-09-06 12:33:27 +0530

    Add files via upload

---
+++ mode.py
@@ -0,0 +1,8 @@
+import keyboard
+import time
+keyboard.wait('win')
+'''keyboard.press_and_release('t')
+time.sleep(0.1)
+keyboard.write('/gamemode spectator')
+time.sleep(0.1)'''
+keyboard.press_and_release('l')
